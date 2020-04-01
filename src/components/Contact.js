c8c9f7d2a845a7ecf8cda919d463da40bfd953e6
helder-hanka <hfernandes238@gmail.com>
2020-04-01 20:44:14 +0200

    modified

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import "./Contact.css"
+
+const oneline = true;
+
 const Contact = () => {
     return (
-        <div className="contact">
-            <div className ="clearfix">
-                <img src="https://randomuser.me/api/portraits/women/85.jpg" alt="avatar"/>
-                <div className="about">
-                    <div className="message">Salut je m'appelle</div>
-                    <div className="name">Kim Sanchez</div>
+        <div className="Contact">
+                <img className ="avatar" src="https://randomuser.me/api/portraits/women/85.jpg" alt="photo"/>
+            <div>
+                <div className="name">Kim Sanchez</div>
+                <div className="status">
+                    <div className={oneline ? 'status-online' : 'status-offline'}></div>
+                    <p className="status-text">{oneline ? 'Oneline' : 'status-offline'}</p>
                 </div>
-                <i className="fa fa-star"></i>
             </div>
         </div>
     )
